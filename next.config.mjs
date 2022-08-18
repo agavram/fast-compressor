c745b72448f39cfa9e4522fa6f91fa6bf9a812f5
Adrian Avram <Adrian_Avram@Outlook.com>
2022-08-17 22:50:43 -0700

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,37 @@
+import { env } from "./src/env/server.mjs";
+
+/**
+ * Don't be scared of the generics here.
+ * All they do is to give us autocompletion when using this.
+ *
+ * @template {import('next').NextConfig} T
+ * @param {T} config - A generic parameter that flows through to the return type
+ * @constraint {{import('next').NextConfig}}
+ */
+function defineNextConfig(config)
+{
+  config.headers = async () =>
+  {
+    return [
+      {
+        source: "/", // change to appropriate path
+        headers: [
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+        ],
+      },
+    ];
+  };
+  return config;
+}
+
+export default defineNextConfig({
+  reactStrictMode: true,
+  swcMinify: true,
+});
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundColor: {
+        'black': '#1C1D29'
+      }
+    },
+  },
+  plugins: [],
+};
